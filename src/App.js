dfab5f4d7ca3a25bbde42c886fb353c128b52fc2
jph-sw <jph.sw@proton.me>
2023-11-11 15:09:28 +0100

    changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [message, setData] = useState(null);
+  const [show, setShow] = useState(false);
+  const [time, setTime] = useState();
+  const [pv, setPV] = useState();
+  const [ewe, setEwe] = useState();
+
+  useEffect(() => {
+    fetch("http://pv-api.onrender.com/data")
+      .then((res) => res.json())
+      .then((data) => {
+        setTime(data.StatusSNS.Time);
+        setPV(data.StatusSNS.PV);
+        setEwe(data.StatusSNS.EWE);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-header">Stromverbrauch</h1>
+      <p>Uhrzeit: {time}</p>
+      <p>Ewe: {JSON.stringify(ewe)}</p>
+      <p>PV: {JSON.stringify(pv)}</p>
     </div>
   );
 }
